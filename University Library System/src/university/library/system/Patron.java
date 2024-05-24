85043c78dd3c7200a7442aefa10b0f3ab85f6d5f
miphangtm <85862012+miphangtm@users.noreply.github.com>
2024-05-24 23:53:12 +1000

    Add files via upload

---
+++ University Library System/src/university/library/system/Patron.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package university.library.system;
+
+/**
+ *
+ * @author DELL
+ */
+public class Patron {
+    private String name;
+    private String address;
+    private String telephoneNumber;
+}
